cab07de252a4abccc3057bd3ef2bd01e886efc44
Joe Spicuzza <spicuzza21@gmail.com>
2020-09-29 20:29:15 -0400

    more prompts

--- src/server/utils/tests.js
+++ src/server/utils/tests.js
@@ -189,6 +189,152 @@ function test5287e858c6b5a9678200083c(func) {
   return true;
 }
 
+function test55b42574ff091733d900002f(func) {
+  const res = func(['Tom', 'Eric', 'Gabe']);
+  if (res !== ['Eric', 'Gave']) return { res, expected: ['Eric', 'Gave'] };
+  const res2 = func(['Alex']);
+  if (res2 !== ['Alex']) return { res: res2, expected: ['Alex'] };
+  return true;
+}
+
+function test523a86aa4230ebb5420001e1(func) {
+  const res = func('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']);
+  if (res !== ['carer', 'racer']) return { res, expected: ['carer', 'racer'] };
+  const res2 = func('abcde', ['edcba']);
+  if (res2 !== ['edcba']) return { res: res2, expected: ['edcba'] };
+  const res3 = func('hello', ['goodbye']);
+  if (res3 !== []) return { res: res3, expected: [] };
+  return true;
+}
+
+function test541c8630095125aba6000c00(func) {
+  const res = func(17);
+  if (res !== 8) return { res, expected: 8 };
+  const res2 = func(942);
+  if (res2 !== 6) return { res: res2, expected: 6 };
+  const res3 = func(493193);
+  if (res3 !== 2) return { res: res3, expected: 2 };
+  return true;
+}
+
+function test59f7fc109f0e86d705000043(func) {
+  const res = func('123');
+  if (res !== true) return { res, expected: true };
+  const res2 = func('33333333');
+  if (res2 !== true) return { res: res2, expected: true };
+  const res3 = func('13');
+  if (res3 !== false) return { res: res3, expected: false };
+  return true;
+}
+
+function test55c45be3b2079eccff00010f(func) {
+  const res = func('is2 Thi1s T4est 3a');
+  if (res !== 'Thi1s is2 3a T4est') return { res, expected: 'Thi1s is2 3a T4est' };
+  const res2 = func('4of Fo1r pe6ople g3ood th5e the2');
+  if (res2 !== 'Fo1r the2 g3ood 4of th5e pe6ople') return { res: res2, expected: 'Fo1r the2 g3ood 4of th5e pe6ople' };
+  const res3 = func('one1 thr3ee 2two');
+  if (res3 !== 'one1 2two thr3ee') return { res: res3, expected: 'one1 2two thr3ee' };
+  return true;
+}
+
+function test545cedaa9943f7fe7b000048(func) {
+  const res = func('The quick brown fox jumps over the lazy dog');
+  if (res !== true) return { res, expected: true };
+  const res2 = func('abcdefghijklmnopqrstuvwxy');
+  if (res2 !== false) return { res: res2, expected: false };
+  return true;
+}
+
+function test54b42f9314d9229fd6000d9c(func) {
+  const res = func('recede');
+  if (res !== '()()()') return { res, expected: '()()()' };
+  const res2 = func('Success');
+  if (res2 !== ')())())') return { res: res2, expected: ')())())' };
+  return true;
+}
+
+function test54da5a58ea159efa38000836(func) {
+  const res = func([1, 1, 1, 2, 2]);
+  if (res !== 1) return { res, expected: 1 };
+  const res2 = func([1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 3]);
+  if (res2 !== 2) return { res: res2, expected: 2 };
+  return true;
+}
+
+function test558fc85d8fd1938afb000014(func) {
+  const res = func([19, 5, 42, 2, 77]);
+  if (res !== 7) return { res, expected: 7 };
+  const res2 = func([100, 9, 300, 5678]);
+  if (res2 !== 109) return { res: res2, expected: 109 };
+  return true;
+}
+
+function test5a15a4db06d5b6d33c000018(func) {
+  const res = func([1, [2, [3, [4]]]]);
+  if (res !== 10) return { res, expected: 10 };
+  const res2 = func([1, 10, [2, 5]]);
+  if (res2 !== 18) return { res: res2, expected: 18 };
+  return true;
+}
+
+function test52597aa56021e91c93000cb0(func) {
+  const res = func([false, 1, 0, 1, 2, 0, 1, 3, 'a']);
+  if (res !== [false, 1, 1, 2, 1, 3, 'a', 0, 0]) return { res, expected: [false, 1, 1, 2, 1, 3, 'a', 0, 0] };
+  const res2 = func([1, 0, 6, 7, 0, 'g']);
+  if (res2 !== [1, 6, 7, 'g', 0, 0]) return { res: res2, expected: [1, 6, 7, 'g', 0, 0] };
+  return true;
+}
+
+function test52fba66badcd10859f00097e(func) {
+  const res = func('This website is for losers LOL!');
+  if (res !== 'Ths wbst s fr lsrs LL!') return { res, expected: 'Ths wbst s fr lsrs LL!' };
+  const res2 = func('remove my vowels');
+  if (res2 !== 'rmv my vwls') return { res: res2, expected: 'rmv my vwls' };
+  return true;
+}
+
+function test55908aad6620c066bc00002a(func) {
+  const res = func('ooxxooxx');
+  if (res !== true) return { res, expected: true };
+  const res2 = func('xxxxx');
+  if (res2 !== false) return { res: res2, expected: false };
+  return true;
+}
+
+function test5467e4d82edf8bbf40000155(func) {
+  const res = func('42145');
+  if (res !== '54421') return { res, expected: '54421' };
+  const res2 = func('1234567');
+  if (res2 !== '7654321') return { res: res2, expected: '7654321' };
+  return true;
+}
+
+function test57cebe1dc6fdc20c57000ac9(func) {
+  const res = func('find my shortest word');
+  if (res !== 2) return { res, expected: 2 };
+  const res2 = func('aaa bbbbb ccc ddddddd');
+  if (res2 !== 3) return { res: res2, expected: 3 };
+  return true;
+}
+
+function test54ff3102c1bad923760001f3(func) {
+  const res = func('count my vowels');
+  if (res !== 4) return { res, expected: 4 };
+  const res2 = func('abcdefghi');
+  if (res2 !== 3) return { res: res2, expected: 3 };
+  const res3 = func('bhtgfd rwq');
+  if (res3 !== 0) return { res: res3, expected: 0 };
+  return true;
+}
+
+function test57eadb7ecd143f4c9c0000a3(func) {
+  const res = func('Test Name');
+  if (res !== 'T.S') return { res, expected: 'T.S' };
+  const res2 = func('First Last');
+  if (res2 !== 'F.L') return { res: res2, expected: 'F.L' };
+  return true;
+}
+
 module.exports = {
   test: {
     test59cfc000aeb2844d16000075,
@@ -212,5 +358,22 @@ module.exports = {
     test57547f9182655569ab0008c4,
     test5727bb0fe81185ae62000ae3,
     test5287e858c6b5a9678200083c,
+    test55b42574ff091733d900002f,
+    test523a86aa4230ebb5420001e1,
+    test541c8630095125aba6000c00,
+    test59f7fc109f0e86d705000043,
+    test55c45be3b2079eccff00010f,
+    test545cedaa9943f7fe7b000048,
+    test54b42f9314d9229fd6000d9c,
+    test54da5a58ea159efa38000836,
+    test558fc85d8fd1938afb000014,
+    test5a15a4db06d5b6d33c000018,
+    test52597aa56021e91c93000cb0,
+    test52fba66badcd10859f00097e,
+    test55908aad6620c066bc00002a,
+    test5467e4d82edf8bbf40000155,
+    test57cebe1dc6fdc20c57000ac9,
+    test54ff3102c1bad923760001f3,
+    test57eadb7ecd143f4c9c0000a3,
   },
 };
